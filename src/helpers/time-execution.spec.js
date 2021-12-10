fab5aa87f960a9e1e9d28308f2d4ea9210154721
Scott Morris <scott.r.morris@gmail.com>
2021-12-09 23:52:13 -0500

    Refactored and added helper unit tests

---
+++ src/helpers/time-execution.spec.js
@@ -0,0 +1,19 @@
+// Dependencies
+
+const timeExecution = require('./time-execution');
+
+// Tests
+
+describe('time-execution.js:', () => {
+  test('when called, it will return an object with the result and duration', () => {
+    const mockFn = jest.fn().mockImplementation((scalar) => scalar * 2);
+    const timedMock = timeExecution(mockFn);
+
+    expect(timedMock(2)).toEqual(
+      expect.objectContaining({
+        result: 4,
+        duration: expect.any(Number),
+      })
+    );
+  });
+});
