867d0dcd01775f12d4c2a0145320bd720e5b4a1e
Scott Morris <smorris2@netjets.com>
2021-12-16 09:48:52 -0500

    Tweaked day 13 output

--- src/day-13/run.js
+++ src/day-13/run.js
@@ -1,5 +1,6 @@
 // Dependencies
 
+const chalk = require('chalk');
 const displayOutput = require('../helpers/display-output');
 const fs = require('../helpers/fs-extravaganza');
 const getInputFile = require('../helpers/get-input-file');
@@ -18,8 +19,10 @@ function main() {
   const result1 = timeExecution(part1)(input);
   const result2 = timeExecution(part2)(input);
 
-  displayOutput(result1, result2);
+  displayOutput(result1, { duration: result2.duration });
 
+  console.log();
+  console.log(chalk.bold.underline.blue('Result #2:'));
   console.log();
   console.log(result2.result);
 }
