f8b69a2c6fda822d9bc4da9337838106d9deee65
Scott Morris <smorris2@netjets.com>
2021-12-16 11:10:52 -0500

    Added logic to remove trailing newline from inputs

--- getinput.sh
+++ getinput.sh
@@ -7,9 +7,20 @@ set -e
 ## Color Variables ##
 
 RED='\033[00;31m'
+GREEN='\033[00;32m'
+BLUE='\033[00;34m'
 BOLD='\033[00;1m'
 BOLD_RED='\033[00;1;31m'
+BOLD_BLUE_UNDERLINE='\033[00;1;4;31m'
 RESET='\033[00;0m'
+
+# Functions
+
+display_usage (){
+    echo -e "\n${BOLD}Usage:${RESET} $0 [dayNumber] \n"
+}
+
+echo ""
 
 # Check to see if arguments weren't given
 if [[ $# -eq 0 ]]; then
@@ -22,6 +33,8 @@ fi
 if [[ ! -f ./session.env ]]; then
     echo -e "${BOLD_RED}ERROR: ${RED}Missing session.env${RESET}"
     exit 0
+else
+    echo -e "${BLUE}Session information found in ${BOLD_BLUE_UNDERLINE}./session.env${RESET}"
 fi
 
 # Get the environment variable
@@ -35,7 +48,17 @@ if [ -f $FILE ]; then
 # Run the `run.js` in the given folder if it can be found
     echo -e "${BOLD}INFO: ${RESET} \"${FILE}\" already exists"
 else
-    echo ""
-    curl https://adventofcode.com/2021/day/$1/input --cookie "session=$SESSION" --output $FILE
-    echo ""
+    URL=https://adventofcode.com/2021/day/$1/input
+    echo -e "${BLUE}Downloading ${BOLD_BLUE_UNDERLINE}${URL}${BLUE} to ${BOLD_BLUE_UNDERLINE}${FILE}${RESET}"
+    curl $URL --cookie "session=$SESSION" --output $FILE &>/dev/null
+
+    LENGTH=$(wc -c <$FILE)
+    if [ "$LENGTH" -ne 0 ] && [ -z "$(tail -c -1 <$FILE)" ]; then
+        # The file ends with a newline or null
+        echo -e "${BLUE}${BOLD_BLUE_UNDERLINE}${FILE}${BLUE} ends with a newline or null. Removing it...${RESET}"
+        dd if=/dev/null of=$FILE obs="$((LENGTH-1))" seek=1 &>/dev/null
+    fi
+
+    echo "";
+    echo -e "${GREEN}Your data for Day ${DAY} is available at ${BOLD_BLUE_UNDERLINE}${FILE}${RESET}"
 fi
